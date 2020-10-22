42b9d15eb7edf3203db9bab511f2a5e0074af8a9
Joohong Park <daleaderprofq@gmail.com>
2020-10-22 20:55:01 +0900

    ft_printf start

---
+++ 42cursus/ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joopark <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 18:49:10 by joopark           #+#    #+#             */
+/*   Updated: 2020/10/22 18:51:05 by joopark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int			ft_printf(const char *fmt);
+
+
+#endif
