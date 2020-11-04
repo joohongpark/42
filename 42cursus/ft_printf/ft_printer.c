d446ce87484f07040a726e914f2ee29dcae1fc1b
joohongpark <daleaderprofq@gmail.com>
2020-11-04 22:25:37 +0900

    부호 있는 정수 예외처리

--- 42cursus/ft_printf/ft_printer.c
+++ 42cursus/ft_printf/ft_printer.c
@@ -6,7 +6,7 @@
 /*   By: joopark <joopark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 01:36:14 by joopark           #+#    #+#             */
-/*   Updated: 2020/11/04 14:50:36 by joopark          ###   ########.fr       */
+/*   Updated: 2020/11/04 21:57:32 by joopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,18 +67,31 @@ int					ft_printint(t_format form, int n)
 	int				intlen;
 	int				space;
 	
-	intlen = ft_intlen(((n < 0) ? ~n + 1 : n), 10, NULL);
+	intlen = ft_intlen(((n < 0) ? ~n + 1 : n) & 0x00000000ffffffffUL, 10, NULL);
+	/*
+	form.precision
+	form.width
+	부호
+	0 또는 공백
+	*/
 	rtn = intlen;
 	rtn += (n < 0) ? 1 : 0;
 	space = form.width - ((form.precision > intlen) ? form.precision : intlen);
 	space -= (n < 0) ? 1 : 0;
-	if (form.set_right == 0)
-		rtn += ft_putchar((form.precision == -2 && form.fill == 1) ? '0' : ' ', space);
+	if (form.set_right == 0 && (form.precision != -2 || form.fill == 0))
+		rtn += ft_putchar(' ', space);
 	if (n < 0)
 		write(1, "-", 1);
 	if (form.precision > intlen)
 		rtn += ft_putchar('0', form.precision - intlen);
-	ft_putnbr_base(((n < 0) ? ~n + 1 : n) & 0x000000007fffffffUL, "0123456789");
+	else if (form.precision == -2 && form.set_right == 0 && form.fill == 1)
+		rtn += ft_putchar('0', space);
+	if (((n < 0) ? ~n + 1 : n) == 0 && form.precision == 0 && form.width == 0)
+		rtn--;
+	else if (((n < 0) ? ~n + 1 : n) == 0 && form.precision == 0)
+		ft_putchar(' ', 1);
+	else
+		ft_putnbr_base(((n < 0) ? ~n + 1 : n) & 0x00000000ffffffffUL, "0123456789");
 	if (form.set_right == 1)
 		rtn += ft_putchar(' ', space);
 	return (rtn);
