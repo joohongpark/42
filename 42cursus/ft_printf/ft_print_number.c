dcd6631c26f52457b7b5463a4043e0c3e3d57e79
joohongpark <daleaderprofq@gmail.com>
2020-11-11 00:08:12 +0900

    정수 출력 코드 예외사항 추가

--- 42cursus/ft_printf/ft_print_number.c
+++ 42cursus/ft_printf/ft_print_number.c
@@ -6,7 +6,7 @@
 /*   By: joopark <joopark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 18:08:28 by joopark           #+#    #+#             */
-/*   Updated: 2020/11/09 16:48:34 by joopark          ###   ########.fr       */
+/*   Updated: 2020/11/10 22:02:56 by joopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,23 @@ int					ft_printint(t_format form, ssize_t n)
 	int				rtn;
 	int				l;
 	int				space;
-	char			c;
+	char			pre;
 	ssize_t			num;
 
 	num = (n < 0) ? ~n + 1 : n;
 	l = ft_nbrlen(num & ft_mask(form.length), 10, NULL);
-	c = form.space ? ' ' : '\0';
-	c = form.plus ? '+' : c;
-	c = (n < 0) ? '-' : c;
-	space = form.width - ((c != '\0') + ((form.prec < l) ? l : form.prec));
+	l = (num == 0 && form.prec == 0) ? 0 : l;
+	pre = form.space ? ' ' : '\0';
+	pre = form.plus ? '+' : pre;
+	pre = (n < 0) ? '-' : pre;
+	space = form.width - ((pre != '\0') + ((form.prec < l) ? l : form.prec));
 	rtn = (!form.r && (!form.fill || form.prec >= 0)) ? ft_putchar(' ', space) : 0;
-	rtn += (c) ? ft_putstr(&c, 1) : 0;
+	rtn += (pre) ? ft_putstr(&pre, 1) : 0;
 	if (form.prec > l)
 		rtn += ft_putchar('0', form.prec - l);
-	else if ((form.width > (l + (c != '\0'))) && (form.prec == -2) && form.fill && !form.r)
-		rtn += ft_putchar('0', form.width - (l + (c != '\0')));
-	if (num == 0 && form.prec == 0 && form.width != 0)
-		rtn += ft_putchar(' ', 1);
-	else if (!(num == 0 && form.prec == 0 && form.width == 0))
+	else if ((form.width > (l + (pre != '\0'))) && (form.prec == -2) && form.fill && !form.r)
+		rtn += ft_putchar('0', form.width - (l + (pre != '\0')));
+	if (!(num == 0 && form.prec == 0))
 		rtn += ft_putnbr_base(num & ft_mask(form.length), "0123456789");
 	rtn += (form.r == 1) ? ft_putchar(' ', space) : 0;
 	return (rtn);
@@ -48,7 +47,8 @@ int					ft_printuint(t_format form, ssize_t n)
 	int				pre;
 
 	l = ft_nbrlen(n & ft_mask(form.length), (form.type == 'u') ? 10 : 16, NULL);
-	pre = (form.prefix && (form.type == 'X' || form.type == 'x')) ? 2 : 0;
+	l = (n == 0 && form.prec == 0) ? 0 : l;
+	pre = (form.prefix && ((form.type == 'X' || form.type == 'x') && n != 0)) ? 2 : 0;
 	space = form.width - (pre + ((form.prec < l) ? l : form.prec));
 	rtn = (!form.r && (!form.fill || form.prec >= 0)) ? ft_putchar(' ', space) : 0;
 	rtn += (pre) ? ft_putstr(form.type == 'X' ? "0X" : "0x", 2) : 0;
@@ -56,13 +56,11 @@ int					ft_printuint(t_format form, ssize_t n)
 		rtn += ft_putchar('0', form.prec - l);
 	else if ((form.width > (l + pre)) && (form.prec == -2) && form.fill && !form.r)
 		rtn += ft_putchar('0', form.width - (l + pre));
-	if (n == 0 && form.prec == 0)
-		rtn += (form.width != 0) ? ft_putchar(' ', 1) : 0;
-	else if (form.type == 'u')
+	if (form.type == 'u' && !(n == 0 && form.prec == 0))
 		rtn += ft_putnbr_base(n & ft_mask(form.length), "0123456789");
-	else if (form.type == 'x')
+	else if (form.type == 'x' && !(n == 0 && form.prec == 0))
 		rtn += ft_putnbr_base(n & ft_mask(form.length), "0123456789abcdef");
-	else if (form.type == 'X')
+	else if (form.type == 'X' && !(n == 0 && form.prec == 0))
 		rtn += ft_putnbr_base(n & ft_mask(form.length), "0123456789ABCDEF");
 	rtn += (form.r == 1) ? ft_putchar(' ', space) : 0;
 	return (rtn);
