151df34a63efd9da9a31ff97c21629423dfdec8b
joohongpark <daleaderprofq@gmail.com>
2021-01-21 09:58:12 +0900

    nasm 경로 수정 및 테스트 케이스 프로그램 수정

--- 42cursus/libasm/test.c
+++ 42cursus/libasm/test.c
@@ -6,7 +6,7 @@
 /*   By: joopark <joopark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 18:21:08 by joopark           #+#    #+#             */
-/*   Updated: 2021/01/19 16:17:23 by joopark          ###   ########.fr       */
+/*   Updated: 2021/01/21 09:52:32 by joopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,20 +35,27 @@ int main (void)
 
 void		strlen_test(void)
 {
-	printf("strlen test\n");
+	printf("\x1b[32m[strlen test]\x1b[0m\n");
 	if (ft_strlen("1234sdfsdf") != strlen("1234sdfsdf"))
 		printf("error at strlen #1\n");
+	else
+		printf("ok #1\n");
 	if (ft_strlen("asdf\0asdf") != strlen("asdf\0asdf"))
 		printf("error at strlen #2\n");
+	else
+		printf("ok #2\n");
 	if (ft_strlen("dpy3ES9wjh49Stq1ayebN2/VmuC93ml8I4AAAggggAACCCCQcgECZ8qJb+wNvMD51z5FaekX8qt//7UbGfZGBs7p2sTUQvGbq1bLZ8ePh73GEzpFidWCRisLO9C30UrD9LixBW7P7Nlz3EA8vmK3morA6c2HaTeIFXQjn8O2rXbTajnLdYChuXOj1xzHC5xTp07R/0hoItEGVop2T/YhgAACCCCAAAIIIFBVAgTOqpKsptf55S9/Ls2a5WnTWHG1eTZHpP1VVlbqd6V+f7Ntgcam2rC+hadOn5KKSxWhN7qRgXP8+LHSsmXLqLVzDz000b3bX9NU1BuYx15u6bLlckqnUYlcUhE4vf6odi//QESR947cvl3nN31A5/m0mljr1+mfLsU7Nl7gfPLJn0nNmjVl0eIlcvbMWe80vhFAAAEEEEAAAQQQSLkAgTPlxDf/DRIJnNan0uujaM13r2ig9S/xyv3H+tf73ddP7tK5Nj/+eLds2/aev0h6/qin9NK/S5cuyRwdhdb6nMZbbPqR6TpokDUntoGGNm/ect0pNvKtjexqiz8c+vuURs5p6W/iu2TJMjl9+vR11x2rNavWXPm41tSu0hrbRJY8/c+CSRqsbVm3foMcPnT4utMGDNA+mj26Rx1cyXsumzIl1qBD112QHQgggAACCCCAAAIIVJEAgbOKIDP5MokEzn/4+7+TkSOHO4Y1a9bJCy++FEYSrzzsYN+G1+fS5uC0uTj9iwXAKToPZyOdOqS4+ISb4sTflzNbp0Ppee89btTXdTrfpg2AZMu92kS1tzZVtfkwN2zYJEVFRW5/ltYCdtG+ljYPp01DYkuigdOC7FNak2jTn7yvg/NYv1LbZ1OylJeVuWs1zc2VyZMmumM+3r3H9eX0D4TUXOcy7d2rp3uXXbs+cufYR8HYMdKmdWs3cNCqt1bLl9+OcGshvm+f3tK9ezd3bLR5ODt07CAjRwyXM2fOyOLF0fuAupP5QAABBBBAAAEEEEAgBQIEzhSgZtolEwmcf/j9S9KxYwf36keOHpVnn/1bt+59xCv3jov8rqNThFiNpDUJXbjwDbG+pf7FmttabaSNjmvNTktLS3Uey4sa9LIlLy/PhTsbcMdqJL1aVwtqEx8c7wZIsmuVaSC8ePGS5Oc3C8132a9fX9d3NNHAadcZN65AWrVqZavaLPm0ZDdsqM9dS+bNny+XKy67/d21JrK/1tpaoK3QfSUlJe65c3TApiY6cJMthYWFOsDTZrduH9b/s2DMGKmj08RYqDQDC8/WVNoC9rb3tsuwoUOi1nCOGTNa2rZtEwrBoYuyggACCCCAAAIIIIBAGgQInGlAvtlvkUjgLCgYI88+8wv3qn/8n5dl5co3w147XnnYwREbFiitmWusJrB169WVfn37Sps2bSQ7u2HobAtzhYWfyj7989ck2gHWp3LQwIHSvn17F+QqK6+I1aJ+qDWLp/R76NDB0rlz54RrOO2aNtemzfuZl5drm25E2t1ak7lTazv998/NbSp9NdA2z893ta92rNW22v137/5Ejh07ZrvCliY6evAD9w+U5jp6sIXVCxcu6AjCJ10t6aWKCpk6ZZK+e7bMmDEzdJ4396eF0zlz5kYdICl0MCsIIIAAAggggAACCKRAgMCZAtRMu2QigdPeOV8DlC3WfDPaEq882jm2r137djL6x6NcgHtt1uzAvppWI2r9M63mL5E+nXZ9C2oW4Lwmt7bv+yzWbzKrZpac04GY4i0uIOtUNfa8/lAa6zxr9tugfr2EwuN9WpN6p/Z/LS4u1v8AWBXrkuxHAAEEEEAAAQQQQCBlAgTOlNFmzoUTDZypfOOJEye4QMtckokpW//WR7Upch1tPmx9X632lAUBBBBAAAEEEEAAgXQLEDjTLX4T3s+ac9bMqiHnzl8I9YNM92tYX02bIsWaka5YsTLdt7/p7ueNtFtUdETWrl130z0/D4wAAggggAACCCCQGQIEzsz4HX8Qb9FLR5YtOnJESs6W/CDe9/u8pNVw9tTReK0Pa3l5+fe5FOcigAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0wP8Dn7jHMPS9tGsAAAAASUVORK5CYII=") != strlen("dpy3ES9wjh49Stq1ayebN2/VmuC93ml8I4AAAggggAACCCCQcgECZ8qJb+wNvMD51z5FaekX8qt//7UbGfZGBs7p2sTUQvGbq1bLZ8ePh73GEzpFidWCRisLO9C30UrD9LixBW7P7Nlz3EA8vmK3morA6c2HaTeIFXQjn8O2rXbTajnLdYChuXOj1xzHC5xTp07R/0hoItEGVop2T/YhgAACCCCAAAIIIFBVAgTOqpKsptf55S9/Ls2a5WnTWHG1eTZHpP1VVlbqd6V+f7Ntgcam2rC+hadOn5KKSxWhN7qRgXP8+LHSsmXLqLVzDz000b3bX9NU1BuYx15u6bLlckqnUYlcUhE4vf6odi//QESR947cvl3nN31A5/m0mljr1+mfLsU7Nl7gfPLJn0nNmjVl0eIlcvbMWe80vhFAAAEEEEAAAQQQSLkAgTPlxDf/DRIJnNan0uujaM13r2ig9S/xyv3H+tf73ddP7tK5Nj/+eLds2/aev0h6/qin9NK/S5cuyRwdhdb6nMZbbPqR6TpokDUntoGGNm/ect0pNvKtjexqiz8c+vuURs5p6W/iu2TJMjl9+vR11x2rNavWXPm41tSu0hrbRJY8/c+CSRqsbVm3foMcPnT4utMGDNA+mj26Rx1cyXsumzIl1qBD112QHQgggAACCCCAAAIIVJEAgbOKIDP5MokEzn/4+7+TkSOHO4Y1a9bJCy++FEYSrzzsYN+G1+fS5uC0uTj9iwXAKToPZyOdOqS4+ISb4sTflzNbp0Ppee89btTXdTrfpg2AZMu92kS1tzZVtfkwN2zYJEVFRW5/ltYCdtG+ljYPp01DYkuigdOC7FNak2jTn7yvg/NYv1LbZ1OylJeVuWs1zc2VyZMmumM+3r3H9eX0D4TUXOcy7d2rp3uXXbs+cufYR8HYMdKmdWs3cNCqt1bLl9+OcGshvm+f3tK9ezd3bLR5ODt07CAjRwyXM2fOyOLF0fuAupP5QAABBBBAAAEEEEAgBQIEzhSgZtolEwmcf/j9S9KxYwf36keOHpVnn/1bt+59xCv3jov8rqNThFiNpDUJXbjwDbG+pf7FmttabaSNjmvNTktLS3Uey4sa9LIlLy/PhTsbcMdqJL1aVwtqEx8c7wZIsmuVaSC8ePGS5Oc3C8132a9fX9d3NNHAadcZN65AWrVqZavaLPm0ZDdsqM9dS+bNny+XKy67/d21JrK/1tpaoK3QfSUlJe65c3TApiY6cJMthYWFOsDTZrduH9b/s2DMGKmj08RYqDQDC8/WVNoC9rb3tsuwoUOi1nCOGTNa2rZtEwrBoYuyggACCCCAAAIIIIBAGgQInGlAvtlvkUjgLCgYI88+8wv3qn/8n5dl5co3w147XnnYwREbFiitmWusJrB169WVfn37Sps2bSQ7u2HobAtzhYWfyj7989ck2gHWp3LQwIHSvn17F+QqK6+I1aJ+qDWLp/R76NDB0rlz54RrOO2aNtemzfuZl5drm25E2t1ak7lTazv998/NbSp9NdA2z893ta92rNW22v137/5Ejh07ZrvCliY6evAD9w+U5jp6sIXVCxcu6AjCJ10t6aWKCpk6ZZK+e7bMmDEzdJ4396eF0zlz5kYdICl0MCsIIIAAAggggAACCKRAgMCZAtRMu2QigdPeOV8DlC3WfDPaEq882jm2r137djL6x6NcgHtt1uzAvppWI2r9M63mL5E+nXZ9C2oW4Lwmt7bv+yzWbzKrZpac04GY4i0uIOtUNfa8/lAa6zxr9tugfr2EwuN9WpN6p/Z/LS4u1v8AWBXrkuxHAAEEEEAAAQQQQCBlAgTOlNFmzoUTDZypfOOJEye4QMtckokpW//WR7Upch1tPmx9X632lAUBBBBAAAEEEEAAgXQLEDjTLX4T3s+ac9bMqiHnzl8I9YNM92tYX02bIsWaka5YsTLdt7/p7ueNtFtUdETWrl130z0/D4wAAggggAACCCCQGQIEzsz4HX8Qb9FLR5YtOnJESs6W/CDe9/u8pNVw9tTReK0Pa3l5+fe5FOcigAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0AIEzaTpORAABBBBAAAEEEEAAAQQQCBIgcAbpUIYAAggggAACCCCAAAIIIJC0wP8Dn7jHMPS9tGsAAAAASUVORK5CYII="))
 		printf("error at strlen #3\n");
+	else
+		printf("ok #3\n");
+	printf("\x1b[32m[strlen test finish]\x1b[0m\n");
 }
 
 void		strcpy_test(void)
 {
 	char	buf_ft_strcpy[1000];
 	char	buf_strcpy[1000];
-	printf("strcpy test\n");
+	printf("\x1b[32m[strcpy test]\x1b[0m\n");
 	if (*ft_strcpy(buf_ft_strcpy, "1234sdfsdf") != *strcpy(buf_strcpy, "1234sdfsdf"))
 		printf("error at strcpy #1\n");
 	printf("ft_strcpy\t: [%s]\n", buf_ft_strcpy);
@@ -57,29 +64,43 @@ void		strcpy_test(void)
 		printf("error at strcpy #2\n");
 	printf("ft_strcpy\t: [%s]\n", buf_ft_strcpy);
 	printf("strcpy\t\t: [%s]\n", buf_strcpy);
+	printf("\x1b[32m[strcpy test finish]\x1b[0m\n");
 }
 
 void		strcmp_test(void)
 {
-	printf("strcmp test\n");
+	printf("\x1b[32m[strcmp test]\x1b[0m\n");
 	if (ft_strcmp("equal", "equal") != strcmp("equal", "equal"))
 		printf("error at strcmp #1\n");
+	else
+		printf("ok #1\n");
 	if (ft_strcmp("not equal", "n0t equal") != strcmp("not equal", "n0t equal"))
 		printf("error at strcmp #2\n");
+	else
+		printf("ok #2\n");
 	if (ft_strcmp("", "") != strcmp("", ""))
 		printf("error at strcmp #3\n");
+	else
+		printf("ok #3\n");
 	if (ft_strcmp("hello!", "hello.") != strcmp("hello!", "hello."))
 		printf("error at strcmp #4\n");
+	else
+		printf("ok #4\n");
 	if (ft_strcmp("hello.", "hello!") != strcmp("hello.", "hello!"))
 		printf("error at strcmp #5\n");
+	else
+		printf("ok #5\n");
 	if (ft_strcmp("he\0llo.", "hello!") != strcmp("he\0llo.", "hello!"))
 		printf("error at strcmp #6\n");
+	else
+		printf("ok #6\n");
+	printf("\x1b[32m[strcmp test finish]\x1b[0m\n");
 }
 
 void		write_test(void)
 {
 	long	rtn;
-	printf("write test\n");
+	printf("\x1b[32m[write test]\x1b[0m\n");
 	write(1, "print string by write\n", strlen("print string by write\n"));
 	ft_write(1, "print string by write\n", strlen("print string by write\n"));
 	rtn = ft_write(-1, "hi", 2);
@@ -87,6 +108,7 @@ void		write_test(void)
 	errno = 0;
 	rtn = write(-1, "hi", 2);
 	printf("[write error test]errno : %d\n", errno);
+	printf("\x1b[32m[write test finish]\x1b[0m\n");
 }
 
 void		read_test(void)
@@ -94,9 +116,11 @@ void		read_test(void)
 	char	buf_ft_read[1000] = {0, };
 	char	buf_read[1000] = {0, };
 	long	rtn;
-	printf("read test\n");
+	printf("\x1b[32m[read test]\x1b[0m\n");
+	printf("\x1b[32mplease type something...\x1b[0m\n");
 	read(0, buf_read, 1000);
 	printf("[read]\n%s\n", buf_read);
+	printf("\x1b[32mplease type something...\x1b[0m\n");
 	ft_read(0, buf_ft_read, 1000);
 	printf("[ft_read]\n%s\n", buf_ft_read);
 	rtn = read(-1, buf_read, 1000);
@@ -104,17 +128,19 @@ void		read_test(void)
 	errno = 0;
 	rtn = ft_read(-1, buf_ft_read, 1000);
 	printf("[ft_read error test]errno : %d\n", errno);
+	printf("\x1b[32m[read test finish]\x1b[0m\n");
 }
 
 void		strdup_test(void)
 {
 	char	*s1;
 	char	*s2;
-	printf("strdup test\n");
+	printf("\x1b[32m[strdup test]\x1b[0m\n");
 	s1 = strdup("copy test #1");
 	s2 = ft_strdup("copy test #1");
 	printf("strdup : [%s]\n", s1);
 	printf("ft_strdup : [%s]\n", s2);
 	free(s1);
 	free(s2);
+	printf("\x1b[32m[strdup test finish]\x1b[0m\n");
 }
--- 42cursus/libasm/test_bonus.c
+++ 42cursus/libasm/test_bonus.c
@@ -6,7 +6,7 @@
 /*   By: joopark <joopark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 18:21:08 by joopark           #+#    #+#             */
-/*   Updated: 2021/01/21 09:28:29 by joopark          ###   ########.fr       */
+/*   Updated: 2021/01/21 09:56:36 by joopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,58 +21,9 @@ void		ft_list_size_test(void);
 void		ft_list_sort_test(void);
 void		ft_list_remove_if_test(void);
 
-void ft_list_remove_if1(t_list **begin_list, void *data_ref,
-							int (*cmp)(), void (*free_fct)(void *));
-void ft_list_remove_if1(t_list **begin_list, void *data_ref,
-							int (*cmp)(), void (*free_fct)(void *))
-{
-	t_list *saved_next;
-
-	if (begin_list == NULL || *begin_list == NULL)
-		return ;
-	if (cmp((*begin_list)->data, data_ref) != 0)
-	{
-		ft_list_remove_if1(&(*begin_list)->next, data_ref, cmp, free_fct);
-		return ;
-	}
-	saved_next = (*begin_list)->next;
-	free_fct((*begin_list)->data);
-	free(*begin_list);
-	*begin_list = saved_next;
-	ft_list_remove_if1(begin_list, data_ref, cmp, free_fct);
-}
-void	ft_list_remove_if2(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *))
-{
-	t_list	*list;
-	t_list	*list2;
-	t_list	*tmp;
-
-	list = *begin_list;
-	while (list && list->next)
-	{
-		if ((*cmp)(list->next->data, data_ref) == 0)
-		{
-			tmp = list->next;
-			list->next = list->next->next;
-			(*free_fct)(tmp->data);
-			free(tmp);
-		}
-		list = list->next;
-	}
-	list2 = *begin_list;
-	if (list2 && (*cmp)(list2->data, data_ref) == 0)
-	{
-		*begin_list = list2->next;
-		(*free_fct)(list2->data);
-		free(list2);
-		*begin_list = NULL;
-	}
-}
-
 int		_cmp(void *i, void *j)
 {
-	return *(int*)i - *(int*)j;
-	//return (*((int *)i) > *((int *)j));
+	return (*((int *)i) > *((int *)j));
 }
 
 int main (void)
@@ -87,6 +38,7 @@ int main (void)
 
 void		ft_atoi_base_test(void)
 {
+	printf("\x1b[32m[ft_atoi_base test]\x1b[0m\n");
 	printf("%d\n", ft_atoi_base("", ""));
 	printf("%d\n", ft_atoi_base("10", ""));
 	printf("%d\n", ft_atoi_base("10111", "\t541"));
@@ -95,6 +47,7 @@ void		ft_atoi_base_test(void)
 	printf("%d\n", ft_atoi_base("-123456789", "0123456789"));
 	printf("%d\n", ft_atoi_base("+2147483647", "0123456789"));
 	printf("%d\n", ft_atoi_base("-2147483648", "0123456789"));
+	printf("\x1b[32m[ft_atoi_base test finish]\x1b[0m\n");
 }
 
 void		ft_list_push_front_test(void)
@@ -104,12 +57,14 @@ void		ft_list_push_front_test(void)
 	char	*c1 = "data #1";
 	char	*c2 = "data #2";
 	t = NULL;
+	printf("\x1b[32m[ft_list_push_front test]\x1b[0m\n");
 
 	ft_list_push_front(&t, c1);
 	ft_list_push_front(&t, c2);
 	t1 = t->next;
 	printf("t_list.data : %s\tt_list.next : %p\n", ((char *)(t1->data)), t1->next);
 	printf("t_list.data : %s\tt_list.next : %p\n", ((char *)(t->data)), t->next);
+	printf("\x1b[32m[ft_list_push_front test finish]\x1b[0m\n");
 }
 
 void		ft_list_size_test(void)
@@ -118,10 +73,12 @@ void		ft_list_size_test(void)
 	char	*c1 = "data #1";
 	char	*c2 = "data #2";
 	t = NULL;
+	printf("\x1b[32m[ft_list_size_test test]\x1b[0m\n");
 
 	ft_list_push_front(&t, c1);
 	ft_list_push_front(&t, c2);
 	printf("list size : %d\n", ft_list_size(t));
+	printf("\x1b[32m[ft_list_size_test test finish]\x1b[0m\n");
 }
 
 void		ft_list_sort_test(void)
@@ -130,13 +87,13 @@ void		ft_list_sort_test(void)
 	t_list	*p;
 	int		arr[9] = {23, 34, 232, -1, 1, 45, 12, 12, 98};
 	t = NULL;
+	printf("\x1b[32m[ft_list_sort test]\x1b[0m\n");
 	for (int i = 0; i < 9; i++)
 	{
 		ft_list_push_front(&t, &arr[i]);
 	}
-	printf("list size : %d\n", ft_list_size(t));
 	p = t;
-	printf("before\n");
+	printf("\x1b[35mbefore\x1b[0m\n");
 	while (p != NULL)
 	{
 		printf("data : %d\n", *((int *)(p->data)));
@@ -144,12 +101,13 @@ void		ft_list_sort_test(void)
 	}
 	ft_list_sort(&t, _cmp);
 	p = t;
-	printf("after\n");
+	printf("\x1b[36mafter\x1b[0m\n");
 	while (p != NULL)
 	{
 		printf("data : %d\n", *((int *)(p->data)));
 		p = p->next;
 	}
+	printf("\x1b[32m[ft_list_sort test finish]\x1b[0m\n");
 }
 
 void		ft_list_remove_if_test(void)
@@ -158,12 +116,13 @@ void		ft_list_remove_if_test(void)
 	t_list	*p;
 	t = NULL;
 
+	printf("\x1b[32m[ft_list_remove_if test]\x1b[0m\n");
 	ft_list_push_front(&t, strdup("String #2"));
 	ft_list_push_front(&t, strdup("String #2"));
 	ft_list_push_front(&t, strdup("String #2"));
 	ft_list_push_front(&t, strdup("String #2"));
 	printf("list size : %d\n", ft_list_size(t));
-	printf("before\n");
+	printf("\x1b[35mbefore\x1b[0m\n");
 	p = t;
 	while (p != NULL)
 	{
@@ -172,11 +131,12 @@ void		ft_list_remove_if_test(void)
 	}
 	//ft_list_sort(&t, _cmp);
 	ft_list_remove_if(&t, "String #2", strcmp, free);
-	printf("after\n");
+	printf("\x1b[36mafter\x1b[0m\n");
 	p = t;
 	while (p != NULL)
 	{
 		printf("data : [%s]\n", ((char *)(p->data)));
 		p = p->next;
 	}
+	printf("\x1b[32m[ft_list_remove_if test finish]\x1b[0m\n");
 }
