9d9e88c8ace920843209676a12e38f642308a172
joohongpark <daleaderprofq@gmail.com>
2021-01-21 12:47:12 +0900

    버그 수정

--- 42cursus/libasm/ft_list_remove_if.s
+++ 42cursus/libasm/ft_list_remove_if.s
@@ -24,7 +24,7 @@ _ft_list_remove_if:		PUSH	RBP						; 프롤로그
 						JNE		_set_di					; 0이 아니면 재귀호출 루틴으로
 						MOV		RAX, QWORD [RBP - 8]	; **begin_list에 접근
 						MOV		RAX, QWORD [RAX]		; *begin_list에 접근
-						ADD		RCX, QWORD [RAX + 8]	; *begin_list->next에 접근
+						MOV		RCX, QWORD [RAX + 8]	; *begin_list->next에 접근
 						MOV		QWORD [RBP - 40], RCX	; next를 지역변수에 잠시 저장
 						MOV		RDI, QWORD [RAX]		; tmp->data를 인수1에 집어넣음
 						CALL	QWORD [RBP - 32]		; *free_fct call
@@ -34,7 +34,7 @@ _ft_list_remove_if:		PUSH	RBP						; 프롤로그
 						MOV		RAX, QWORD [RBP - 8]	; **begin_list에 접근
 						MOV		RCX, QWORD [RBP - 40]	; next 꺼냄
 						MOV		[RAX], RCX				; **begin_list가 가리키는 주소에 next 넣음
-						MOV		RDI, RAX				; **begin_list를 인수1에 집어넣음
+						MOV		RDI, QWORD [RBP - 8]	; **begin_list를 인수1에 집어넣음
 						JMP		_recur1					; 재귀호출 루틴으로
 
 _set_di:				MOV		RAX, QWORD [RBP - 8]	; **begin_list에 접근
