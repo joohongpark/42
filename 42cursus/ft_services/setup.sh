df06f7796717bdc0d23ee48a7893bf9f18511586
joohongpark <daleaderprofq@gmail.com>
2021-02-03 18:44:46 +0900

    ftps, metallb ip설정 적용

---
+++ 42cursus/ft_services/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 1. minikube 시작
+#minikube start
+
+# 2. minikube 환경변수 set
+#eval $(minikube docker-env)
+IP=$(minikube ip)
+
+# 3. 환경설정 파일 내용 변경
+sed -i '' -e "s/CLUSTER_IP_ADDR/$IP/g" srcs/ftps/files/vsftpd.conf
+sed -i '' -e "s/CLUSTER_IP_ADDR/$IP/g" srcs/metallb/metallb.yml
