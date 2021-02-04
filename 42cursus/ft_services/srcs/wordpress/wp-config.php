9eb3c7a4671dd67572aa0eed852777fc877c1f66
joohongpark <daleaderprofq@gmail.com>
2021-02-04 11:52:19 +0900

    1. phpmyadin, wordpress를 mysql에 연동 2. wordpress 사용자 정보 db 삽입 구문 추가

--- 42cursus/ft_services/srcs/wordpress/wp-config.php
+++ 42cursus/ft_services/srcs/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wp_root' );
 define( 'DB_PASSWORD', 'joopark' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql-pod:3306' );
+define( 'DB_HOST', 'mysql-ip:3306' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
