c6a1c0566b95127f1c25aa41a2427245472f356f
joohongpark <daleaderprofq@gmail.com>
2021-02-04 21:00:27 +0900

    nginx 설정

--- 42cursus/ft_services/setup.sh
+++ 42cursus/ft_services/setup.sh
@@ -10,3 +10,4 @@ IP=$(minikube ip)
 # 3. 환경설정 파일 내용 변경
 sed -i '' -e "s/CLUSTER_IP_ADDR/$IP/g" srcs/ftps/files/vsftpd.conf
 sed -i '' -e "s/CLUSTER_IP_ADDR/$IP/g" srcs/metallb/metallb.yml
+sed -i '' -e "s/CLUSTER_IP_ADDR/$IP/g" srcs/nginx/files/https.conf
