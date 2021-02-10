a10bd655088022c3a8e646cb0d0a8fc0df9b7603
joohongpark <daleaderprofq@gmail.com>
2021-02-10 22:49:47 +0900

    프롬프트 생성 후 입력 받아보기

--- 42cursus/minishell/minishell.c
+++ 42cursus/minishell/minishell.c
@@ -6,7 +6,7 @@
 /*   By: joopark <joopark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 01:45:32 by joopark           #+#    #+#             */
-/*   Updated: 2021/02/09 01:49:50 by joopark          ###   ########.fr       */
+/*   Updated: 2021/02/10 22:49:08 by joopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,30 @@
 int				main(int argc, char *argv[], char *envp[])
 {
 	int			p;
+	int			gnl;
+	char		*line;
+	char		*bp;
+
+	bp = NULL;
+	line = NULL;
 
 	p = 0;
 	(void)argc;
 	(void)argv;
+
 	while (envp[p] != NULL)
 	{
 		printf("env : %s\n", envp[p]);
 		p++;
+	}
+	while (1)
+	{
+		ft_putstr_fd("\n$> ", 1);
+		gnl = ft_get_next_line(0, &line, &bp);
+		if (gnl == -1)
+			exit(EXIT_FAILURE);
+		write(1, line, ft_strlen(line));
+		free(line);
 	}
 	return (0);
 }
--- 42cursus/minishell/minishell.h
+++ 42cursus/minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: joopark <joopark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 01:45:40 by joopark           #+#    #+#             */
-/*   Updated: 2021/02/09 01:46:48 by joopark          ###   ########.fr       */
+/*   Updated: 2021/02/10 17:30:26 by joopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 
 # include <libft.h>
 # include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char			*ft_strnstack(char *stack, char *str, size_t n);
+char			*ft_getbuf(ssize_t clr, char **bp);
+int				ft_prechk(int fd, char **line, char **bufrtn, char **bp);
+int				ft_get_next_line(int fd, char **line, char **bp);
 
 #endif
