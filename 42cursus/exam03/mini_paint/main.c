8483159e2d2d93328dd8e857e2a98442468b3272
joohongpark <daleaderprofq@gmail.com>
2021-02-15 00:07:26 +0900

    mini_paint 완성

--- 42cursus/exam03/mini_paint/main.c
+++ 42cursus/exam03/mini_paint/main.c
@@ -4,26 +4,21 @@
 #include <unistd.h>
 #include <math.h>
 
-char		**setmem(int w, int h, char c)
+int			print_error(char *errormsg)
 {
-	char	**rtn;
-	int		y;
+	int		len;
 
-	y = 0;
-	rtn = malloc(sizeof(char *) * h);
-	while (y < h)
-	{
-		rtn[y] = malloc(sizeof(char) * w);
-		memset(rtn[y], c, w);
-		y++;
-	}
-	return (rtn);
+	len = 0;
+	while (errormsg[len] != '\0')
+		len++;
+	write(1, errormsg, len);
+	return (1);
 }
 
 int			main(int argc, char *argv[])
 {
 	FILE	*file; // 파일에 대한 정보를 담기 위한 구조체 포인터
-	char	**canvas; // 출력되는 2차원 배열
+	char	*canvas; // 출력되는 배열
 	int		w;
 	int		h;
 	char	c;
@@ -32,16 +27,24 @@ int			main(int argc, char *argv[])
 	float	r;
 	char	f;
 	float	rp;
+	int		e;
 
-
-	if (!(file = fopen("cmd1", "r"))) // 파일을 열지 못하면 NULL포인터가 반환됨.
-		return (1);
+	if ((argc != 2) || !(file = fopen(argv[1], "r"))) // 파일을 열지 못하면 NULL포인터가 반환됨.
+		return (print_error("Error: argument\n"));
 	if (fscanf(file, "%d %d %c\n", &w, &h, &c) != 3)
+		return (print_error("Error: Operation file corrupted\n"));
+	if (!(w > 0 && w <= 300) || !(h > 0 && h <= 300))
+		return (print_error("Error: Operation file corrupted\n"));
+	if (!(canvas = malloc(sizeof(char) * w * h)))
 		return (1);
-	canvas = setmem(w, h, c);
+	memset(canvas, c, w * h);
 
-	while (fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &r, &f) == 5)
+	while ((e = fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &r, &f)))
 	{
+		if (e > 0 && e < 5)
+			return (print_error("Error: Operation file corrupted\n"));
+		else if (e == -1)
+			break;
 		for (int i = 0; i < h; i++)
 		{
 			for (int j = 0; j < w; j++)
@@ -50,18 +53,21 @@ int			main(int argc, char *argv[])
 				if (rp <= r)
 				{
 					if (c == 'C')
-						canvas[i][j] = f;
+						canvas[i * w + j] = f;
 					else if (c == 'c' && r - rp < 1.0)
-						canvas[i][j] = f;
+						canvas[i * w + j] = f;
+					else if (c != 'c' && c != 'C')
+						return (print_error("Error: Operation file corrupted\n"));
 				}
 			}
 		}
 	}
 	for (int i = 0; i < h; i++)
 	{
-		write(1, canvas[i], w);
+		write(1, &canvas[i * w], w);
 		write(1, "\n", 1);
 	}
+	free(canvas);
 	if (fclose(file) != 0)
 		return (1);
 	return (0);
