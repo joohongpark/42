4b471b47b19bb9beda8a140a09d277e46d2e0608
joohongpark <daleaderprofq@gmail.com>
2021-02-14 22:48:49 +0900

    mini_paint 기능 구현 완료

--- 42cursus/exam03/mini_paint/main.c
+++ 42cursus/exam03/mini_paint/main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
 
-char		*gnl_simple(FILE *file)
+char		**setmem(int w, int h, char c)
 {
-	char	c; // 버퍼
-	char	*rtn; // 리턴할 문자열
-	int		len; // 리턴할 문자열 길이
+	char	**rtn;
+	int		y;
 
-	len = 0;
-	rtn = NULL;
-	while (1)
+	y = 0;
+	rtn = malloc(sizeof(char *) * h);
+	while (y < h)
 	{
-		if (fread(&c, 1, 1, file) != 1)
-			break;
-		if (c == '\n' || c == '\0')
-			break;
-		if (!(rtn = realloc(rtn, len++)))
-			return (NULL);
-		rtn[len - 1] = c;
+		rtn[y] = malloc(sizeof(char) * w);
+		memset(rtn[y], c, w);
+		y++;
 	}
 	return (rtn);
 }
@@ -25,13 +23,44 @@ char		*gnl_simple(FILE *file)
 int			main(int argc, char *argv[])
 {
 	FILE	*file; // 파일에 대한 정보를 담기 위한 구조체 포인터
-	char	*line;
+	char	**canvas; // 출력되는 2차원 배열
+	int		w;
+	int		h;
+	char	c;
+	float	x;
+	float	y;
+	float	r;
+	char	f;
+	float	rp;
+
 
 	if (!(file = fopen("cmd1", "r"))) // 파일을 열지 못하면 NULL포인터가 반환됨.
 		return (1);
-	while ((line = gnl_simple(file)))
+	if (fscanf(file, "%d %d %c\n", &w, &h, &c) != 3)
+		return (1);
+	canvas = setmem(w, h, c);
+
+	while (fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &r, &f) == 5)
 	{
-		printf("line : [%s]\n", line);
+		for (int i = 0; i < h; i++)
+		{
+			for (int j = 0; j < w; j++)
+			{
+				rp =  sqrtf((powf(y - i, 2.0) + powf(x - j, 2.0)));
+				if (rp <= r)
+				{
+					if (c == 'C')
+						canvas[i][j] = f;
+					else if (c == 'c' && r - rp < 1.0)
+						canvas[i][j] = f;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < h; i++)
+	{
+		write(1, canvas[i], w);
+		write(1, "\n", 1);
 	}
 	if (fclose(file) != 0)
 		return (1);
