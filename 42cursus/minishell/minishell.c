949732247fa39d12ef732f7f4879f542415577e7
joohongpark <daleaderprofq@gmail.com>
2021-02-16 12:58:15 +0900

    fork/exec test

--- 42cursus/minishell/minishell.c
+++ 42cursus/minishell/minishell.c
@@ -6,7 +6,7 @@
 /*   By: joopark <joopark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 01:45:32 by joopark           #+#    #+#             */
-/*   Updated: 2021/02/10 22:49:08 by joopark          ###   ########.fr       */
+/*   Updated: 2021/02/10 23:52:22 by joopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,21 @@
 
 int				main(int argc, char *argv[], char *envp[])
 {
-	int			p;
-	int			gnl;
-	char		*line;
-	char		*bp;
-
-	bp = NULL;
-	line = NULL;
-
-	p = 0;
-	(void)argc;
-	(void)argv;
-
-	while (envp[p] != NULL)
-	{
-		printf("env : %s\n", envp[p]);
-		p++;
-	}
-	while (1)
-	{
-		ft_putstr_fd("\n$> ", 1);
-		gnl = ft_get_next_line(0, &line, &bp);
-		if (gnl == -1)
-			exit(EXIT_FAILURE);
-		write(1, line, ft_strlen(line));
-		free(line);
-	}
+	pid_t		pid;
+	(void) argc;
+	(void) argv;
+	char *arg1 = "/bin/sleep";
+	char *arg2 = "10";
+	char *arg[3];
+	arg[0] = arg1;
+	arg[1] = arg2;
+	arg[2] = NULL;
+	
+	printf("1. before exec/fork\n");
+	pid = fork();
+	if (pid == 0)
+		execve("/bin/sleep", arg, envp);
+	sleep(10);
+	printf("2. after exec\n");
 	return (0);
 }
